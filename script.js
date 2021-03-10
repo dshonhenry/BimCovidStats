e3cbc00ef9a1aac9f6b95ee4fe49ab847d7438db
RedZelios <dshonamhenry@gmail.com>
2021-03-10 17:51:11 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var key = "1yHk5gHZ2yuNyIuggGdiGrjHSXESWn7r-pcWFtJmiGug";
+
+
+async function getData() {
+    var start = new Date(2021, 0, 26);
+    var today = new Date();
+    var timeDif =  Math.floor((today.getTime() - start.getTime())/(1000*3600*24)) + 2;
+    var url = `https://docs.google.com/spreadsheet/pub?key=${key}&range=B${timeDif}:K${timeDif}&output=csv`;
+    var data = await fetch(url).then(response => response.text());
+    if (data)
+        populate(data);
+    else {
+        timeDif--;
+        var url = `https://docs.google.com/spreadsheet/pub?key=${key}&range=B${timeDif}:K${timeDif}&output=csv`;
+        data = await fetch(url).then(response => response.text());
+        populate(data);
+    }
+}
+
+async function populate(dataString){
+    var data = dataString.split(',');
+    $('#active').html(data[2]);
+    $('#cases').html(data[9].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    $('#vaccines').html(data[8]);
+    $('#deaths').html(data[4]);
+};
+
+window.addEventListener("load", getData);
